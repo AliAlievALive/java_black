b094c24a2268db99d2250b4e26b59c76c8f92930
almiev <madarat@mail.ru>
2021-11-07 18:02:32 +0300

    Generics: Parameterized method

---
+++ src/generics/ParameterizedMethod.java
@@ -0,0 +1,20 @@
+package generics;
+
+import java.util.ArrayList;
+
+public class ParameterizedMethod {
+    public static void main(String[] args) {
+        ArrayList<Integer> al1 = new ArrayList<>();
+        al1.add(1);
+        al1.add(20);
+        al1.add(5);
+        int a = GenMethod.getSecondElement(al1);
+        System.out.println(a);
+    }
+}
+
+class GenMethod {
+    public static <T> T getSecondElement(ArrayList<T> arrayList) {
+        return arrayList.get(1);
+    }
+}
