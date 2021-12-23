d35144f10d093d3e4974c46ea66cd6ae8186a720
AliAlievALive <madarat@mail.ru>
2021-12-23 15:08:46 +0300

    static Nested class example added lesson 36

---
+++ src/collection/nested_classes/Test.java
@@ -0,0 +1,7 @@
+package collection.nested_classes;
+
+public class Test {
+    static class A {
+
+    }
+}
---
+++ src/collection/nested_classes/static_nested_class/Car.java
@@ -0,0 +1,53 @@
+package collection.nested_classes.static_nested_class;
+
+public class Car {
+    String color;
+    int doorCount;
+    Engine engine;
+    private static int a;
+
+    public Car(String color, int doorCount, Engine engine) {
+        this.color = color;
+        this.doorCount = doorCount;
+        this.engine = engine;
+    }
+
+    void meth() {
+        Engine engine = new Engine(200);
+        System.out.println(engine.horsePower);
+    }
+
+    @Override
+    public String toString() {
+        return "My car{" +
+                "color='" + color + '\'' +
+                ", doorCount=" + doorCount +
+                ", engine=" + engine +
+                '}';
+    }
+
+    public static class Engine {
+        private int horsePower;
+
+        public Engine(int horsePower) {
+            System.out.println(a);
+            this.horsePower = horsePower;
+        }
+
+        @Override
+        public String toString() {
+            return "My engine {" +
+                    "horsePower=" + horsePower +
+                    '}';
+        }
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        Car.Engine engine = new Car.Engine(256);
+        System.out.println(engine);
+        Car car = new Car("red", 2, engine);
+        System.out.println(car);
+    }
+}
