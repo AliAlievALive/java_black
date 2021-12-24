3051d5ee0303fec0c6827a09526b8d8bcab89ade
AliAlievALive <madarat@mail.ru>
2021-12-24 18:34:47 +0300

    Streams. peek() example lesson 56

--- src/stream/Test6.java
+++ src/stream/Test6.java
@@ -6,7 +6,6 @@ import java.util.stream.Stream;
 public class Test6 {
     public static void main(String[] args) {
         Stream<Integer> stream5 = Stream.of(1, 2, 3, 4, 5, 1, 2, 3);
-//        System.out.println(stream5.count()); Нельзя переиспользовать стримы
-        System.out.println(stream5.distinct().count());
+        System.out.println(stream5.distinct().peek(System.out::println).count());
     }
 }
