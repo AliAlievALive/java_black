5bb3c1096c6b5914de789043bd5f982ca8161b0a
AliAlievALive <madarat@mail.ru>
2021-12-26 08:05:21 +0300

    Streams. flatMap() example lesson 57

---
+++ src/stream/Test7.java
@@ -0,0 +1,47 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test7 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
+        Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
+        Student st4 = new Student("Petr", 'm', 35, 4, 7);
+        Student st5 = new Student("Mariya", 'f', 23, 3, 9.1);
+
+        Faculty f1 = new Faculty("Economics");
+        Faculty f2 = new Faculty("Applied mathematics");
+        f1.addStudentsOnFaculty(st1);
+        f1.addStudentsOnFaculty(st2);
+        f1.addStudentsOnFaculty(st3);
+        f2.addStudentsOnFaculty(st4);
+        f2.addStudentsOnFaculty(st5);
+
+        List<Faculty> facultyList = new ArrayList<>();
+        facultyList.add(f1);
+        facultyList.add(f2);
+
+        facultyList.stream().flatMap(faculty -> faculty.getStudentsOnFaculty().stream())
+                .forEach(student -> System.out.println(student.getName()));
+    }
+}
+
+class Faculty{
+    String name;
+    List<Student> studentsOnFaculty;
+
+    public Faculty(String name) {
+        this.name = name;
+        this.studentsOnFaculty = new ArrayList<>();
+    }
+
+    public List<Student> getStudentsOnFaculty() {
+        return studentsOnFaculty;
+    }
+
+    public void addStudentsOnFaculty(Student student) {
+        this.studentsOnFaculty.add(student);
+    }
+}
