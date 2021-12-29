d9093bc8f7bd8b05d2818912da49e6c61928b7d8
AliAlievALive <madarat@mail.ru>
2021-12-29 11:07:32 +0300

    Serialization lesson 97 exercise 2

--- src/work_with_files/programmer1/SerializationEx2.java
+++ src/work_with_files/programmer1/SerializationEx2.java
@@ -5,7 +5,9 @@ import java.io.*;
 public class SerializationEx2 {
     public static void main(String[] args) {
         Car car = new Car("Mercedes-Benz", "black");
-        Employee employee = new Employee("Mariya", "IT", 28, 500, car);
+        Employee employee = new Employee("Mariya", "Ivanova","IT",
+//                28,
+                500, car);
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("excluded_files\\employee2.bin"))) {
             outputStream.writeObject(employee);
             System.out.println("Done!!!");
