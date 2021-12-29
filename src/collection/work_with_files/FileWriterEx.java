1614156c5fb8de79c4597a00ec86e779ff34cb06
AliAlievALive <madarat@mail.ru>
2021-12-29 08:09:45 +0300

    FileWriter example lesson 90

---
+++ src/collection/work_with_files/FileWriterEx.java
@@ -0,0 +1,31 @@
+package collection.work_with_files;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterEx {
+    public static void main(String[] args) throws IOException {
+        String rubai = "Много лет размышлял я над жизнью земной.\n" +
+                "Непонятного нет для меня под луной\n" +
+                "Мне известно, что мне ничего не известно!\n" +
+                "Вот последняя правда, открытая мной.\n";
+        String s = "Privet";
+        FileWriter fileWriter = null;
+        try {
+//            fileWriter = new FileWriter("C:\\Users\\Almir_Almiev\\Desktop\\FileForWrite.txt");
+            fileWriter = new FileWriter("FileForWrite.txt", true);
+//            for (int i = 0; i < rubai.length(); i++) {
+//                fileWriter.write(rubai.charAt(i));
+//            }
+//            fileWriter.write(rubai);
+            fileWriter.write(s);
+            System.out.println("Done!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            fileWriter.close();
+        }
+    }
+}
