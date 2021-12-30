f340902b3f5d3a22cd422b902b0eee7841ade7d6
AliAlievALive <madarat@mail.ru>
2021-12-30 09:04:36 +0300

    RegEx example lesson 109 chapter 2

--- src/regex/RegexEx2.java
+++ src/regex/RegexEx2.java
@@ -14,8 +14,26 @@ public class RegexEx2 {
 //        String s1 = "abcd abce abc5abcg6abch";
 //        Pattern pattern1 = Pattern.compile("abc[^e-g4-7]");
 
-        String s1 = "abcd abce abc5abcg6abch";
-        Pattern pattern1 = Pattern.compile("abc(e|5)");
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc(e|5)");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc.");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("6abch$");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\D");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\W");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("\\w+");
+
+        String s1 = "poka acb Ali dom kino";
+        Pattern pattern1 = Pattern.compile("\\w{4}");
 
         Matcher matcher = pattern1.matcher(s1);
         while (matcher.find()) {
